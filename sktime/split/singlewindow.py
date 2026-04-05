8bf4f3b461742cc4e56f66f30fa562eb99333cb7
Merugu Sourish <sourishmerugu@gmail.com>
2026-04-05 06:24:00 +0530

    fix docstring formatting in _split

--- sktime/split/singlewindow.py
+++ sktime/split/singlewindow.py
@@ -112,17 +112,18 @@ class SingleWindowSplitter(BaseSplitter):
 
     def _split(self, y: pd.Index) -> SPLIT_GENERATOR_TYPE:
         """Split time series into a single train/test window.
+
         Parameters
         ----------
         y : pd.Index
-        Index of the time series to split.
+            Index of the time series to split.
 
         Yields
         ------
         training_window : np.ndarray
-        iloc indices of the training window.
+            iloc indices of the training window.
         test_window : np.ndarray
-        iloc indices of the test window.
+            iloc indices of the test window.
         """
         n_timepoints = y.shape[0]
         window_length = check_window_length(self.window_length, n_timepoints)
